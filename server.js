bab79c26acca5d3e0078075fdbfcfa66868df192
ARGHA SEN <arghasen10@gmail.com>
2021-08-19 21:41:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ if (process.argv.length != 4) {
 }
 // For demo purposes we ignore self-signed certificate
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
-
+//My comment
 // Node imports
 var fast2sms = require("fast-two-sms");
 require("dotenv").config();
